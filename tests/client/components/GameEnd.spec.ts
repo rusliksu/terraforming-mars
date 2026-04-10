f03b5ca7ae3719eb4fc036b5f23fce9b59f9f2b1
rusliksu <25394128+rusliksu@users.noreply.github.com>
2026-04-10 15:43:58 +0300

    chore: polish game end rematch cleanup

--- tests/client/components/GameEnd.spec.ts
+++ tests/client/components/GameEnd.spec.ts
@@ -16,5 +16,9 @@ describe('GameEnd', () => {
     expect(wrapper.exists()).to.be.true;
     expect(wrapper.text()).to.contain('Rematch (same setup)');
     expect(wrapper.text()).not.to.contain('Spectator:');
+    const rematchLink = wrapper.findAll('a').find((link) => link.text().includes('Rematch (same setup)'));
+    expect(rematchLink).to.not.be.undefined;
+    expect(rematchLink?.attributes('title')).to.eq('Start a new game with the same initial setup');
+    expect(rematchLink?.attributes('href')).to.contain('new-game?cloneGameId=');
   });
 });

000db6a5ee04376927082f4cec6e183410e95d16
rusliksu <25394128+rusliksu@users.noreply.github.com>
2026-05-16 22:12:31 +0300

    Use strict escape velocity option check

--- src/server/game/calculateVictoryPoints.ts
+++ src/server/game/calculateVictoryPoints.ts
@@ -89,7 +89,7 @@ export function calculateVictoryPoints(player: IPlayer) {
   }
 
   // Escape velocity VP penalty
-  if (player.game.gameOptions.escapeVelocity != null) {
+  if (player.game.gameOptions.escapeVelocity !== null && player.game.gameOptions.escapeVelocity !== undefined) {
     const options = player.game.gameOptions.escapeVelocity;
 
     const elapsedTimeMinutes = player.timer.getElapsedTimeInMinutes();
